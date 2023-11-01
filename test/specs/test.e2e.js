c8d4510c0569f19f9499e37ed69bf64298180017
amit1992lu <amit.luthra@univarsolutions.com>
2023-11-01 17:53:58 +0530

    error

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -7,7 +7,7 @@ describe('My Login application', () => {
         await LoginPage.open()
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toBeExistin
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!')
     })
